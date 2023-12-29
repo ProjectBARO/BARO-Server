9764caec25538dc7cbb4e391767696204fc212b4
Yehyeok Bang <qkddpgur318@gmail.com>
2023-12-29 23:20:12 +0900

    [#2] feat: 사용자 model, repository 추가
    - 사용자는 고유한 이메일을 가집니다.

---
+++ models/User.go
@@ -0,0 +1,10 @@
+package models
+
+type User struct {
+	ID       uint   `json:"id" gorm:"primary_key"`
+	Name     string `json:"name"`
+	Nickname string `json:"nickname"`
+	Email    string `json:"email" gorm:"unique"`
+	Age      int    `json:"age"`
+	Gender   string `json:"gender"`
+}
