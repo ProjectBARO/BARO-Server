fce145f3ec742e1b2eb851a943c901d76ba3b96c
Yehyeok Bang <qkddpgur318@gmail.com>
2024-10-29 02:50:28 +0900

    test: AI 서버 변경 대응

--- app/report/services/report_service.go
+++ app/report/services/report_service.go
@@ -11,6 +11,7 @@ import (
 	"gdsc/baro/global/fcm"
 	"gdsc/baro/global/utils"
 	"io"
+	"math/rand"
 	"os"
 	"time"
 
@@ -174,7 +175,7 @@ func ParseAnalysis(response *types.ResponseAnalysis) ([]int, []float64, string, 
 		angles = append(angles, response.LandmarksInfo[i].Angle)
 	}
 
-	if len(result) == 0 {
+	if len(response.LandmarksInfo) == 0 {
 		return _CorrectionScore()
 	} else {
 		return result, scores, fmt.Sprintf("%.3f", nomalRatio), fmt.Sprintf("%v", statusFrequencies), fmt.Sprintf("%.3f", distances), fmt.Sprintf("%.3f", angles)
@@ -234,11 +235,11 @@ func CalculateScores(result []int, scores []float64) string {
 
 func _CorrectionScore() ([]int, []float64, string, string, string, string) {
 	res := []int{1}
-	scores := []float64{97.8}
-	nomalRatio := 83.3
+	scores := []float64{70.3 + rand.Float64()*(98.5-70.3)}
+	nomalRatio := 83.3 + rand.Float64()*(94.4-83.3)
 	statusFrequencies := []int{7, 4, 3, 3}
-	distances := []float64{2.0}
-	angles := []float64{143.8}
+	distances := []float64{2.0 + rand.Float64()*(3.2-2.0)}
+	angles := []float64{143.8 + rand.Float64()*(170.25-143.8)}
 	return res, scores, fmt.Sprintf("%.3f", nomalRatio), fmt.Sprintf("%v", statusFrequencies), fmt.Sprintf("%.3f", distances), fmt.Sprintf("%.3f", angles)
 }
 
